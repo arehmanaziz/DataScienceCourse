455c16e60c094a9fb515660c1ace58ec403cb042
Abdur Rehman Aziz <78584456+arehmanaziz@users.noreply.github.com>
2023-12-21 16:46:31 +0500

    Delete class2 directory

--- class2/bmi_calculator.py
+++
@@ -1,6 +0,0 @@
-weight = float(input("Input Weight: "))
-height = float(input("Input Height: "))
-
-bmi = weight / height ** 2
-print(bmi)
-
--- class2/tip_calculator.py
+++
@@ -1,8 +0,0 @@
-bill = float(input("Input bill: "))
-people = int(input("Input number of people: "))
-tip_percent = float(input("Tip percent: "))
-total_bill = bill + (bill*(tip_percent/100))
-each_person = total_bill / people
-round_bill = format(each_person, ".2f")
-
-print(f"Every person should pay {round_bill}Rs. amount.")

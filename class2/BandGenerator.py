beb684551e26f166783fc29f42c55c17008e6314
Abdur Rehman Aziz <78584456+arehmanaziz@users.noreply.github.com>
2023-12-21 15:45:23 +0500

    Add files via upload

---
+++ class2/BandGenerator.py
@@ -0,0 +1,9 @@
+# Band Generator Program
+
+print("Welcome to band generator program.")
+
+city_name = input("Write your city name where you grew up: ")
+
+pet_name = input("Write your pet name: ")
+
+name = city_name + pet_name
